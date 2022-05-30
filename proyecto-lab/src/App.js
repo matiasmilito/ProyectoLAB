85b8d1a5207b4a5f4fbe379f09337ac352763143
gaston <gnghigo@mail.austral.edu.ar>
2022-05-29 23:10:04 -0300

    - Solucionado problema del nombre del Navbar

--- proyecto-lab/src/App.js
+++ proyecto-lab/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "./components/navbar/NavBar";
+import NavBar from "./components/Navbar/NavBar";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './App.css';
 import Staff from "./routes/staff/staff";
