de55790be5340ae53cb56fd3f57dec84059c33f3
matiasmilito <matiasmilito99@gmail.com>
2022-06-02 18:06:15 -0300

    httpFunctions y Login

--- proyecto-lab/src/routes/login/login.js
+++ proyecto-lab/src/routes/login/login.js
@@ -1,28 +1,46 @@
 import React from "react";
 import "./login.css"
-// import { useState, useEffect} from "react";
+import { useState } from "react";
+import {httpPost} from "../../utils/httpFunctions";
 
 const Login = () => {
 
-  // const [username, setUsername] = useState();
-  // const [password, setPassword] = useState();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const login = (e) => {
+      e.preventDefault()
+      httpPost('api/login/', {username: username, password: password}).then((res) => {
+          localStorage.setItem('token', res.data.access)
+      })
+  }
 
 
   return (
       <div className="login">
-        <div className="form-container">
+        <form className="form-container" onSubmit={login}>
           <div className="username-container">
             <label className="username-label">Username</label>
-            <input type="text" placeholder="tuUsuarioEjemplo"/>
+            <input
+                type="text"
+                placeholder="tuUsuarioEjemplo"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div className="password-container">
             <label className="password-label">Password</label>
-            <input type="password" placeholder="Ingrese su contraseña"/>
+            <input
+                type="password"
+                placeholder="Ingrese su contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           {/*<div className="button-container">*/}
             <button type="submit" className="login-button">Ingresar</button>
           {/*</div>*/}
-        </div>
+        </form>
       </div>
   );
 }
--- proyecto-lab/src/utils/httpFunctions.js
+++ proyecto-lab/src/utils/httpFunctions.js
@@ -1,4 +1,13 @@
+
+
 const axios = require('axios');
 
 const baseUrl = 'http://localhost:3000/';
 
+export const httpGet = async (endpoint) => {
+  return axios.get(baseUrl + endpoint)
+}
+
+export const httpPost = async (endpoint, data) => {
+    return axios.post(baseUrl + endpoint, data)
+}
