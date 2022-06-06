cee28980c0adbb21dd95cf46e751b3889906dfd3
gaston <gnghigo@mail.austral.edu.ar>
2022-06-05 22:13:26 -0300

    agregue estilos a la home page

--- proyecto-lab/src/routes/home/home.js
+++ proyecto-lab/src/routes/home/home.js
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from "./home.module.css";
-import {Button} from '../../components/button/Button';
+import {Link} from 'react-router-dom';
+import { Button } from '../../components/button/Button'
+
 
 const Home = () => {
+
   return (
     <div>
       <div className={styles.contenedor}>
@@ -10,9 +13,9 @@ const Home = () => {
         <div className={styles.contenedorTitulo}>
           <h2 className={styles.titulo}>Sanatorio Rosario</h2>
           <h3 className={styles.subTitulo}>Deja tu salud en nuestras manos!</h3>
-          <div className="button-container">
-            <Button>Ver Mas</Button>
-          </div>
+          <Button className={styles.buttonContainer}>
+            <Link to={'/sanatorio'} className={styles.verMasLink}>Ver Mas</Link>
+          </Button>
         </div>
       </div>
     </div>
