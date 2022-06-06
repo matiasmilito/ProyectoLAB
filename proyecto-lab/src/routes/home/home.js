4167bb463b460ff654714e63caf40fbfecacdfdb
matiasmilito <matiasmilito99@gmail.com>
2022-06-06 19:21:13 -0300

    Register

--- proyecto-lab/src/routes/home/home.js
+++ proyecto-lab/src/routes/home/home.js
@@ -1,11 +1,8 @@
 import React from "react";
 import styles from "./home.module.css";
-import {Link} from 'react-router-dom';
-import { Button } from '../../components/button/Button'
-
+import {Button} from '../../components/button/Button';
 
 const Home = () => {
-
   return (
     <div>
       <div className={styles.contenedor}>
@@ -13,9 +10,9 @@ const Home = () => {
         <div className={styles.contenedorTitulo}>
           <h2 className={styles.titulo}>Sanatorio Rosario</h2>
           <h3 className={styles.subTitulo}>Deja tu salud en nuestras manos!</h3>
-          <Button className={styles.buttonContainer}>
-            <Link to={'/sanatorio'} className={styles.verMasLink}>Ver Mas</Link>
-          </Button>
+          <div className="button-container">
+            <Button>Ver Mas</Button>
+          </div>
         </div>
       </div>
     </div>
