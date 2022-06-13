2e3cbf775e2952688a469e5d063fa67f7a3ce123
matiasmilito <matiasmilito99@gmail.com>
2022-06-13 19:08:14 -0300

    Inicio de sesion y register

--- proyecto-lab/src/routes/turnos/turnos.js
+++ proyecto-lab/src/routes/turnos/turnos.js
@@ -2,8 +2,29 @@ import React from "react";
 
 const Turnos = () => {
   return (
-      <h1>Turnos</h1>
-  )
+      <div>
+        <div>
+          <h1>Turnos</h1>
+        </div>
+        <div>
+          <label>Especialidad</label>
+          <select id="especialidad" name="especialidad">
+            <option value="Neurologia">Neurologia</option>
+            <option value="Traumatologia">Traumatologia</option>
+            <option value="Kinesiologia">Kinesiologia</option>
+            <option value="Oncología">Oncología</option>
+          </select>
+        </div>
+        <div>
+          <label>Profesional</label>
+          <select id="profesionales" name="profesionales">
+            <option value="JavierPerez">Javier Perez</option>
+            <option value="GonzaloRamirez">Gonzalo Ramirez</option>
+            <option value="Sabrina Fernandez">Sabrina Fernandez</option>
+          </select>
+        </div>
+      </div>
+  );
 }
 
 export default Turnos;
