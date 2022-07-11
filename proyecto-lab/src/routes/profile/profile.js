17265ba37ec6f6a01f876d48b073b5e970fef60c
bdinamo <briandinamo@hotmail.com>
2022-07-11 19:51:18 -0300

    profile, utils

--- proyecto-lab/src/routes/profile/profile.js
+++ proyecto-lab/src/routes/profile/profile.js
@@ -6,15 +6,17 @@ const Profile = () => {
     const [userData, setUserData] = useState({})
 
     useEffect(() => {
-        httpGet('api/me/').then((res) => setUserData(res.data))
+        httpGet('api/me/').then((res) =>
+        /*console.log(res.data))*/
+        setUserData(res.data))
     }, [])
 
-    const fetchUser = () => {
+    /*const fetchUser = () => {
         httpGet('api/login/')
             .then((res) => setUserData(res.data))
       }
-    
-      useEffect(fetchUser, [])
+
+      useEffect(fetchUser, [])*/
     
 
 
@@ -22,9 +24,10 @@ const Profile = () => {
         <div>
         <h2>Hola! Este es mi perfil</h2>
         <div>
-            <h3>Mi nombre y apellido es {userData.first_name} {userData.last_name}</h3>
+            <h3>Mi nombre y apellido es {userData.first_name}  {userData.last_name}</h3>
             <h3>Mi correo es {userData.email}</h3>
             <h3>Mi nombre de usuario es {userData.username}</h3>
+
         </div>
       </div>       
     )
--- proyecto-lab/src/utils/httpFunctions.js
+++ proyecto-lab/src/utils/httpFunctions.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 const baseUrl = ' http://127.0.0.1:8000/';
 
 export const httpGet = async (endpoint) => {
-  return axios.get(baseUrl + endpoint, {
-      headers: {
-          authorization: localStorage.getItem('token')
-      }
-  })
+    return axios.get(baseUrl + endpoint, {
+        headers: {
+            authorization: 'Bearer ' + localStorage.getItem('token')
+        }
+    })
 }
 
 export const httpPost = async (endpoint, data) => {
