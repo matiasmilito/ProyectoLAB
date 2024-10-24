714f4d5a8f38937c2784a4cbea78fc56ba61c4d9
gaston <gnghigo@mail.austral.edu.ar>
2024-10-23 22:56:55 -0300

    - Agregado 'proyectolab.onrender.com' al listado de hosts permitidos

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = getenv("SECRET_JWT_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["proyectolab.onrender.com"]
 
 
 # Application definition
