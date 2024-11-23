5e17995b6357c6bb705976f6be2735d28dfe81b1
gaston <gnghigo@mail.austral.edu.ar>
2024-11-23 20:13:17 -0300

    - mejorado aplicacion de reactbrowser

--- proyecto-lab/src/App.js
+++ proyecto-lab/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./components/Navbar/NavBar";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import './App.css';
 import Staff from "./routes/staff/staff";
 import Home from "./routes/home/home";
@@ -12,20 +12,18 @@ import Profile from "./routes/profile/profile";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavBar/>
-        <Routes>
-          <Route exact={true} path="/" element={<Home/>} />
-          <Route exact={true} path="/staff" element={<Staff/>} />
-          <Route exact={true} path="/turnos" element={<Turnos/>} />
-          <Route exact={true} path="/login" element={<Login/>} />
-          <Route exact={true} path="/Register" element={<Register/>} />
-          <Route exact={true} path="/profile" element={<Profile/>} />
-        </Routes>
-        <Footer/>
-      </div>
-    </Router>
+    <div>
+      <NavBar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/staff" element={<Staff/>} />
+        <Route exact path="/turnos" element={<Turnos/>} />
+        <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/Register" element={<Register/>} />
+        <Route exact path="/profile" element={<Profile/>} />
+      </Routes>
+      <Footer/>
+    </div>
   );
 }
 
