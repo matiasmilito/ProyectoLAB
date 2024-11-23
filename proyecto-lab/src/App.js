1aa3556604be6b39fb53ef2293a585e45460ff22
gaston <gnghigo@mail.austral.edu.ar>
2024-11-23 20:29:54 -0300

    - mas cambios para solucionar problema de ruteo

--- proyecto-lab/src/App.js
+++ proyecto-lab/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./components/Navbar/NavBar";
 import {Routes, Route} from "react-router-dom";
 import './App.css';
@@ -11,17 +11,23 @@ import Register from "./routes/register/Register";
 import Profile from "./routes/profile/profile";
 
 function App() {
+  useEffect(() => {
+    console.log('App component mounted');
+    console.log('Current path:', window.location.pathname);
+  }, []);
+
   return (
     <div>
       <NavBar/>
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/staff" element={<Staff/>} />
-        <Route exact path="/turnos" element={<Turnos/>} />
-        <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/Register" element={<Register/>} />
-        <Route exact path="/profile" element={<Profile/>} />
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/staff" element={<Staff/>} />
+          <Route exact path="/turnos" element={<Turnos/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/Register" element={<Register/>} />
+          <Route exact path="/profile" element={<Profile/>} />
+          <Route path="*" element={<div>404 - Not Found</div>} />
+        </Routes>
       <Footer/>
     </div>
   );
