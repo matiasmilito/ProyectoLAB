753470304e8f6d9dd64e9269f3db0df75961f54b
gaston <gnghigo@mail.austral.edu.ar>
2024-11-23 21:29:08 -0300

    - asd

--- proyecto-lab/src/components/AuthRoute.js
+++ proyecto-lab/src/components/AuthRoute.js
@@ -1,5 +1,5 @@
 import { Navigate, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const AuthRoute = ({ children }) => {
     const [loading, setLoading] = useState(true);
